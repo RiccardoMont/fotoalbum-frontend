cbdd9f44d70901cd87fefe2900be4e1a0c6fb803
RiccardoMont <riccardomontalto94@gmail.com>
2024-06-17 18:26:27 +0200

    Added AppCategoriesRowsBody component

--- src/components/AppCategoriesRowsBody.vue
+++ src/components/AppCategoriesRowsBody.vue
@@ -1,15 +1,60 @@
 <script>
+import axios from 'axios';
+import { state } from '../state';
+import AppCardPhoto from './AppCardPhoto.vue';
+
 
 export default {
     name: 'AppCategoriesRowsBody',
     data() {
         return {
+            state,
+            url_categories: `${state.base_api_url}${state.categories_endpoint}`,
+            category_rows: []
         }
+    },
+    components: {
+        AppCardPhoto
+    },
+    methods: {
+        fetchCategoriesRows(url) {
+            axios
+                .get(url)
+                .then(resp => {
+                    this.category_rows = [...resp.data.results];
+                    console.log(this.category_rows);
+
+                    this.category_rows.forEach(category => {
+                        const url = `${state.base_api_url}${state.categories_endpoint}/filter?category=${category.id}`;
+                        state.fetchDataSearchGun(url);
+                    })
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+        }
+    },
+    mounted() {
+        this.fetchCategoriesRows(this.url_categories);
     }
 }
 </script>
 <template>
-
+    <div v-for="gun_cat in state.gun_photos">
+        <div v-for="single_cat in this.category_rows.slice(0, this.category_rows.length)"> {{ single_cat.id ==
+            gun_cat[0].pivot.category_id ? single_cat.title : ''}}</div>
+        <div class="original-scrollable-row">
+            <AppCardPhoto v-for="photo in gun_cat" :photo="photo"></AppCardPhoto>
+        </div>
+    </div>
 </template>
 <style scoped>
+
+.original-scrollable-row {
+    display: flex;
+    flex-wrap: nowrap;
+    gap: 5px;
+    height: 200px;
+    overflow: auto;
+}
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,21 +1,18 @@
 <script>
-//import axios from 'axios';
 import axios from 'axios';
 import { state } from '../state';
-import AppCardPhoto from './AppCardPhoto.vue';
+
 import AppCarousel from './AppCarousel.vue';
 import AppCategoriesFilter from './AppCategoriesFilter.vue';
+import AppCategoriesRowsBody from './AppCategoriesRowsBody.vue';
 
 
 export default {
     name: 'AppMain',
     data() {
         return {
-            state,
-            //Array vuoto di supporto per filtrare tramite le categorie
-            category_ids: [],
-            url_categories: `${state.base_api_url}${state.categories_endpoint}`,
-            category_rows: []
+            state
+            
 
 
         }
@@ -23,7 +20,8 @@ export default {
     components: {
         AppCategoriesFilter,
         AppCarousel,
-        AppCardPhoto
+        AppCategoriesRowsBody
+        
     },
     methods: {
         //Funzione per popolare sia le categorie che le foto al caricamento iniziale
@@ -50,34 +48,12 @@ export default {
                 state.fetchDataSearch(url);
             }
         },
-        fetchCategoriesRows(url) {
-            axios
-                .get(url)
-                .then(resp => {
-                    this.category_rows = [...resp.data.results];
-                    console.log(this.category_rows);
 
-                    this.category_rows.forEach(category => {
-                        const url = `${state.base_api_url}${state.categories_endpoint}/filter?category=${category.id}`;
-                        state.fetchDataSearchGun(url);
-                    })
-                })
-                .catch(err => {
-                    console.error(err);
-                })
-        },
-
-        categoryRows() {
-            const url_categories = `${state.base_api_url}${state.categories_endpoint}`;
-            console.log(url_categories);
-            this.fetchCategoriesRows(url_categories);
-            console.log(state.category_rows);
-        }
     },
     mounted() {
 
         this.initialApi();
-        this.fetchCategoriesRows(this.url_categories);
+        
         
 
 
@@ -91,12 +67,7 @@ export default {
         <AppCarousel></AppCarousel>
         <AppCategoriesFilter></AppCategoriesFilter>
         <button id="highlighted-btn" class="my-4 btn btn-primary" @click="this.highlightedFilter()">Highlighted</button>
-        <div v-for="gun_cat in state.gun_photos">
-            <div v-for="single_cat in this.category_rows.slice(0, this.category_rows.length)"> {{ single_cat.id == gun_cat[0].pivot.category_id ? single_cat.title : ''}}</div>
-            <div class="original-scrollable-row">
-                <AppCardPhoto v-for="photo in gun_cat" :photo="photo"></AppCardPhoto>
-            </div>
-        </div>
+        <AppCategoriesRowsBody></AppCategoriesRowsBody>
     </div>
 </template>
 <style scoped>
@@ -104,32 +75,4 @@ export default {
     background-color: var(--bg-softblue);
 }
 
-.h200 {
-    height: 200px;
-}
-
-.width-content {
-    width: max-content;
-}
-
-.oxa {
-    overflow-x: hidden !important;
-
-}
-
-.scrollable-row {
-    display: flex;
-    flex-wrap: nowrap;
-    white-space: nowrap;
-    overflow: auto;
-}
-
-.original-scrollable-row {
-    display: flex;
-    flex-wrap: nowrap;
-    gap: 5px;
-    height: 200px;
-    /*white-space: nowrap;*/
-    overflow: auto;
-}
 </style>
