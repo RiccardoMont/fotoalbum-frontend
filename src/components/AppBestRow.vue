99e795292f5e202cc21a3a6342191a8774033d2b
RiccardoMont <riccardomontalto94@gmail.com>
2024-06-19 13:17:39 +0200

    Added modals on click to allow the visualization of the single photo.

--- src/components/AppBestRow.vue
+++ src/components/AppBestRow.vue
@@ -27,17 +27,44 @@ export default {
         </div>
         <!--<div class="original-scrollable-row">-->
         <div class="best-row">
-            <div class="w-50 big-card" v-for="photo in state.bestshoots_photos">
-                <div class="mask">
-                    <img :src="state.base_api_url + '/storage/' + photo.photos[0].image" alt="">
-                    <div class="badge"><span>{{ photo.title }}</span></div>
+            <div class="w-50" v-for="photo in state.bestshoots_photos" data-bs-toggle="modal"
+                :data-bs-target="'#' + photo.photos[0].id + 'best'">
+                <div class="big-card">
+                    <div class="mask">
+                        <img :src="state.base_api_url + '/storage/' + photo.photos[0].image" alt="">
+                        <div class="badge"><span>{{ photo.title }}</span></div>
+                    </div>
+                    <div class="card-img-overlay">
+                        <h5>{{ photo.photos[0].title }}</h5>
+                        <p><small>Last updated 3 mins ago</small></p>
+                    </div>
                 </div>
-                <div class="card-img-overlay">
-                    <h5>{{ photo.photos[0].title }}</h5>
-                    <p><small>Last updated 3 mins ago</small></p>
+                <div class="modal fade" :id="photo.photos[0].id + 'best'" tabindex="-1"
+                    :aria-labelledby="photo.photos[0].id + 'modalLabel'" aria-hidden="true">
+                    <div class="modal-dialog modal-fullscreen">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body d-flex justify-content-between">
+                                <img class="h-100"
+                                    :src="photo.photos[0].image.startsWith('https://') ? photo.photos[0].image : state.base_api_url + '/storage/' + photo.photos[0].image"
+                                    alt="">
+                                <div class="px-4 d-flex flex-column justify-content-between">
+
+
+                                    <div class="texts">
+                                        <h3>{{ photo.photos[0].title }}</h3>
+                                        <p>{{ photo.photos[0].description }}</p>
+                                    </div>
+                                    <div class="badge_big"><span>{{ photo.title }}</span></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <!--<AppCardPhoto v-for="photo in state.bestshoots_photos" :photo="photo.photos[0]"></AppCardPhoto>-->
         </div>
 
     </div>
@@ -55,8 +82,8 @@ export default {
 
 .big-card {
     cursor: pointer;
-    position: relative  ;
-    
+    position: relative;
+
     & .mask {
         opacity: 1;
     }
@@ -66,6 +93,15 @@ export default {
         opacity: 0;
         color: var(--text-white);
     }
+}
+
+.modal-content {
+    background-color: var(--bg-softblue);
+
+    & .texts {
+        text-align: end;
+    }
+
 }
 
 .big-card:hover {
@@ -110,5 +146,12 @@ export default {
     right: 0px;
     --bs-badge-font-size: 1em;
     --bs-badge-border-radius: 0;
+}
+
+.badge_big {
+    background-color: var(--button-mag);
+    padding: 1rem;
+    font-size: 22px;
+    border-radius: 16px;
 }
 </style>
--- src/components/AppCardPhoto.vue
+++ src/components/AppCardPhoto.vue
@@ -14,7 +14,7 @@ export default {
 }
 </script>
 <template>
-    <div v-if="photo.published" class="card">
+    <div v-if="photo.published" class="card" data-bs-toggle="modal" :data-bs-target="'#' + photo.id">
         <img :src="photo.image.startsWith('https://') ? photo.image : state.base_api_url + '/storage/' + photo.image"
             alt="">
         <div class="card-img-overlay">
@@ -22,10 +22,29 @@ export default {
             <p><small>Last updated 3 mins ago</small></p>
         </div>
     </div>
+    <div class="modal fade" :id="photo.id" tabindex="-1" :aria-labelledby="photo.id + 'modal'" aria-hidden="true">
+        <div class="modal-dialog modal-fullscreen">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body d-flex justify-content-between">
+                    <img class="h-100" :src="photo.image.startsWith('https://') ? photo.image : state.base_api_url + '/storage/' + photo.image"
+                        alt="">
+                    <div class="texts px-4">
+                        <h3>{{ photo.title }}</h3>
+                        <p>{{ photo.description }}</p>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
 </template>
 <style scoped>
 .card {
-    
+
     border-radius: 0;
     background-color: inherit;
     cursor: pointer;
@@ -58,5 +77,14 @@ export default {
         opacity: 1;
 
     }
+}
+
+.modal-content{
+    background-color: var(--bg-softblue);
+
+    & .texts{
+        text-align: end;
+    }
+
 }
 </style>
